e30b640d8444ddc2055c48c301f42d77b0c4e705
alejo <alejonc15@gmail.com>
2019-01-12 22:53:30 -0500

    Demo de un simple mantenimiento de empleado

---
+++ src/com/fisi/model/EmpleadoImpMySQL.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fisi.model;
+
+import com.fisi.domain.Empleado;
+import com.fisi.service.IEmpleado;
+import java.util.List;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class EmpleadoImpMySQL implements IEmpleado {
+
+    @Override
+    public List<Empleado> traerEmpleados() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void registrarEmpleado(Empleado empleado) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void modificarEmpleado(Empleado empleado) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminarEmpleado(Empleado empleado) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Empleado buscarEmpleado(String cod) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
